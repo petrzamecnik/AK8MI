2432593d17f60eaa3c5db9e5cd8ca44845e766b6
petrz <zamecnikpetr@protonmail.com>
2024-04-14 13:08:03 +0200

    created test functions

---
+++ benchmarking.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+# Test Functions
+def dejong1(x):
+    x = np.array(x)
+    return np.sum(x ** 2)
+
+
+def dejong2(x):
+    # f(x⃗) = Σ (x_i^2 - 10 * cos(2π * x_i) + 10) pro i = 1, 2, ..., n
+
+    x = np.array(x)
+    return np.sum(x ** 2 - 10 * np.cos(2 * np.pi * x) + 10)
+
+
+def Schweffel(x):
+    # (x⃗) = -Σ (x_i * sin(sqrt(|x_i|))) pro i = 1, 2, ..., n
+    x = np.array(x)
+    return -np.sum(x * np.sin(np.sqrt(np.abs(x))))
+
+
+# Implementations
+test_arr = [1.2, -3.4, 2.1, -0.7, 4.3]
+res1 = dejong1(test_arr)
+res2 = dejong2(test_arr)
+print(res1)
+print(res2)
